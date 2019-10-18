be8dfc950818ef8036a49fab4e801485e188d09a
Kutay Agbal <kutayagbal@gmail.com>
2019-10-18 11:07:25 +0300

    run as  suite

--- ch04/ex_4_3-5_mock_integration_test/src/test/java/com/example/demo/Ex43MockIntegrationTestApplicationTests.java
+++ ch04/ex_4_3-5_mock_integration_test/src/test/java/com/example/demo/Ex43MockIntegrationTestApplicationTests.java
@@ -2,15 +2,15 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ AccountServiceTest.class })
 @SpringBootTest
 public class Ex43MockIntegrationTestApplicationTests {
-
+	
 	@Test
 	public void contextLoads() {
 	}
-
 }
