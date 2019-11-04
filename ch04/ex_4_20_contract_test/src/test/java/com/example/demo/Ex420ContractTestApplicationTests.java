c2040b09a72c7119a0397f08987e931070f0c22b
Kutay Agbal <kutayagbal@gmail.com>
2019-11-04 14:07:49 +0300

    contract test

--- ch04/ex_4_20_contract_test/src/test/java/com/example/demo/Ex420ContractTestApplicationTests.java
+++ ch04/ex_4_20_contract_test/src/test/java/com/example/demo/Ex420ContractTestApplicationTests.java
@@ -23,7 +23,7 @@ public class Ex420ContractTestApplicationTests {
 	public void contractTest() throws Exception {
 		ConsumerUser u = userServiceClient.callUserService();
 
-		assertThat(u.getId()).isPositive();
+		assertThat(u.getId()).isGreaterThanOrEqualTo(0);
 		assertThat(u.getUsername()).matches("[A-Za-z0-9]+");
 		assertThat(u.getFirstName()).matches("[A-Za-z]+");
 		assertThat(u.getLastName()).matches("[A-Za-z]+");
